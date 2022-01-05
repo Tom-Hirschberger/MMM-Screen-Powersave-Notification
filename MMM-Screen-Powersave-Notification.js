24339a9cf8c4ef11713b3f2459a8e0a6265381cf
Tom Hirschberger <github@hirschberger.eu>
2022-01-05 22:25:04 +0100

    fixed hiding of modules instead of shut off the screen

--- MMM-Screen-Powersave-Notification.js
+++ MMM-Screen-Powersave-Notification.js
@@ -101,10 +101,12 @@ Module.register('MMM-Screen-Powersave-Notification', {
     self.hiddenModules = []
     var allModules = MM.getModules()
     allModules.enumerate(function(curModule){
-      var callback = function(){}
-      var options = {lockString: self.identifier}
-      self.hiddenModules.push(curModule)
-      curModule.hide(self.config.hideAnimationSpeed,callback,options)
+      if (!curModule.hidden){
+        var callback = function(){}
+        var options = {lockString: self.identifier}
+        self.hiddenModules.push(curModule)
+        curModule.hide(self.config.hideAnimationSpeed,callback,options)
+      }
     })
   },
 
@@ -132,7 +134,7 @@ Module.register('MMM-Screen-Powersave-Notification', {
       this.updateDom()
     } else if (notification === 'SCREEN_HIDE_MODULES'){
       self.sendNotification("DISABLE_PROFILE_TIMERS")
-      if (self.changeToProfile !== null){
+      if (self.config.changeToProfile !== null){
         if(self.profileHistory[1] === self.config.changeToProfile){
           self.profileHistory[0] = self.profileHistory[1]
         }
@@ -142,7 +144,7 @@ Module.register('MMM-Screen-Powersave-Notification', {
       }
     } else if (notification === 'SCREEN_SHOW_MODULES'){
       self.sendNotification("ENABLE_PROFILE_TIMERS")
-      if (self.changeToProfile !== null){
+      if (self.config.changeToProfile !== null){
         self.sendNotification("CURRENT_PROFILE", self.profileHistory[0])
       } else {
         self.showModules()
